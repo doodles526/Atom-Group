eac98363a69f0b710a7d804a86720c404f736971
Joshua Deare <dearej@onid.orst.edu>
2012-03-12 00:04:47 -0700

    added ssh support closes #6

---
+++ puppet/modules/ssh/manifests/init.pp
@@ -0,0 +1,32 @@
+class ssh {
+
+case $operatingsystem {
+    /RedHat|CentOS/: {
+      package { "openssh":
+        ensure => installed,
+        alias => "ssh",
+      }
+    }
+    /Debian|Ubuntu/: {
+      package { "ssh":
+        ensure => installed
+      }
+    }
+  }
+
+  service { "ssh":
+    ensure => running,
+    hasrestart => true,
+    pattern => "/usr/sbin/sshd",
+    require => Package["ssh"],
+  }
+
+  file { "/etc/ssh/ssh_known_hosts":
+    ensure => present,
+    mode => 0644,
+    owner => "root",
+  }
+
+
+
+}    
